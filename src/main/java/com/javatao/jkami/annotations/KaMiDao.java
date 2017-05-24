bdd44e9cdef3c48cc5f912947805c9dbe277a96f
lftao <javatao@qq.com>
2017-05-24 14:47:41 +0800

    识别spring 注解，支持自定义bean的name

--- src/main/java/com/javatao/jkami/annotations/KaMiDao.java
+++ src/main/java/com/javatao/jkami/annotations/KaMiDao.java
@@ -1,10 +1,13 @@
 package com.javatao.jkami.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
 
 /**
  * KaMiDao
@@ -12,6 +15,8 @@ import java.lang.annotation.Target;
  * @author tao
  */
 @Inherited
+@Documented
+@Component
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface KaMiDao {
